6e08f608c8a45e392afd3843bfea75bc33f07568
Eric <eric.tan@tokoin.io>
2022-04-21 14:55:11 +0800

    master

--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+
+// STORE -> GLOBALIZED STATE
+
+// ACTION INCREMENT (What u want to do?)
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+// REDUCER (Controller?)
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+}
+
+let store = createStore(counter);
+
+// Display in console
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH (Call ACTION?)
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
